0bbb186016d97d8e0a18255900496eddc7e3ac30
BorjaGV <borjavalcarce97@gmail.com>
2024-03-20 23:55:53 +0100

    note adapter and note list fragment 80% done

--- app/src/main/java/com/example/taskpad/fragments/NoteListFragment.kt
+++ app/src/main/java/com/example/taskpad/fragments/NoteListFragment.kt
@@ -1,22 +1,136 @@
 package com.example.taskpad.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.taskpad.R
+import com.example.taskpad.databinding.FragmentNoteListBinding
+import com.example.taskpad.databinding.FragmentTaskListBinding
+import com.example.taskpad.utils.NoteAdapter
+import com.example.taskpad.utils.NoteData
+import com.example.taskpad.utils.TaskAdapter
+import com.example.taskpad.utils.TaskData
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
-class NoteListFragment : Fragment() {
+class NoteListFragment : Fragment(), NoteAdapter.NoteAdapterClicksInterface,
+AddNewNotePopupFragment.UpdateDialogBtnClickListener {
 
-
+    private lateinit var auth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var binding: FragmentNoteListBinding
+    private lateinit var adapter: NoteAdapter
+    private var popupFragment: AddNewNotePopupFragment? = null
+    private lateinit var mList: MutableList<NoteData>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_list, container, false)
+        binding = FragmentNoteListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        getDataFromFirebase()
+
+
+    }
+
+    private fun init(view: View) {
+        auth = FirebaseAuth.getInstance()
+        databaseReference = FirebaseDatabase.getInstance().reference
+            .child("users")
+            .child(auth.currentUser?.uid.toString())
+            .child("notes")
+
+
+
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        mList   = mutableListOf()
+        adapter = NoteAdapter(mList)
+        adapter.setListener(this)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun getDataFromFirebase() {
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onDataChange(snapshot: DataSnapshot) {
+                mList.clear()
+                for (noteSnapshot in snapshot.children) {
+                    val noteId = noteSnapshot.key ?: ""
+                    val noteValue = noteSnapshot.child("note").getValue(String::class.java) ?: ""
+                    val noteDescValue = noteSnapshot.child("noteDesc").getValue(String::class.java) ?: ""
+                    val note = NoteData(noteId, noteValue, noteDescValue)
+                    mList.add(note)
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+
+
+    override fun onUpdateNote(
+        noteData: NoteData,
+        newNoteTitleEt: TextInputEditText,
+        newNoteDescEt: TextInputEditText
+    ) {
+        val newTitle = newNoteTitleEt.text.toString().trim()
+        if (newTitle.isNotEmpty()) {
+            val noteRef = databaseReference.child(noteData.noteId).child("note").child("description")
+            noteRef.setValue(newTitle).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(context, "Updated Successfully", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, task.exception.toString(), Toast.LENGTH_SHORT).show()
+                }
+                popupFragment!!.dismiss()
+            }
+        } else {
+            Toast.makeText(context, "Title cannot be empty", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDeleteNoteBtnClicked(noteData: NoteData) {
+        databaseReference.child(noteData.noteId).removeValue().addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(context, "Deleted Successfully", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onEditNoteBtnClicked(noteData: NoteData) {
+        if (popupFragment != null)
+            childFragmentManager.beginTransaction().remove(popupFragment!!).commit()
+
+        popupFragment = AddNewNotePopupFragment.newInstance(noteData.noteId, noteData.note, noteData.noteDesc)
+        popupFragment!!.setListener(this)
+        popupFragment!!.setNoteAction("Update")
+        popupFragment!!.show(childFragmentManager, AddNewNotePopupFragment.TAG)
     }
 
 
---
+++ app/src/main/java/com/example/taskpad/utils/NoteAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.taskpad.utils
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taskpad.databinding.NoteListItemBinding
+
+
+
+class NoteAdapter (private val list: MutableList<NoteData>) :
+    RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    private var listener: NoteAdapterClicksInterface? = null
+    private  val TAG = "NoteAdapter"
+    fun setListener(listener: NoteAdapterClicksInterface) {
+        this.listener = listener
+    }
+
+    inner class NoteViewHolder(val binding: NoteListItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val binding = NoteListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NoteViewHolder(binding)
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        with(holder){
+            with(list[position]){
+                binding.noteTitle.text = this.note
+                Log.d(TAG, "onBindViewHolder: "+this)
+                binding.deleteNote.setOnClickListener {
+                    listener?.onDeleteNoteBtnClicked(this)
+                }
+
+                binding.noteContent.setOnClickListener {
+                    listener?.onEditNoteBtnClicked(this)
+                }
+            }
+        }
+
+    }
+
+    interface NoteAdapterClicksInterface {
+        fun onDeleteNoteBtnClicked(noteData: NoteData)
+        fun onEditNoteBtnClicked(noteData: NoteData)
+    }
+}
+
